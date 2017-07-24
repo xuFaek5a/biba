411434e6db00a8d1dc3c2da94d1dec12f8324b6d
tm <tmtm9010@gmail.com>
2017-07-24 18:02:22 +0300

    Added sprite class

--- biba.js
+++ biba.js
@@ -5,20 +5,34 @@ function init(){
     forkImg.src='fork.svg';
 }
 
+function sprite(ctx, img, x, y){
+    this.ctx = ctx;
+    this.img = img;
+    if (x && y) {
+        this.x = x;
+        this.y = y;
+    }
+    else {
+        this.x = 0;
+        this.y = 0;
+    }
+    this.draw = function(){
+        this.ctx.drawImage(this.img, this.x, this.y);
+    } 
+    this.move = function(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    this.collision = (x, y) => x > this.x && x < this.x + this.img.naturalWidth && y > this.y && y < this.y + this.img.naturalHeight;
+}
+
 function draw(){
     var canvas = document.getElementById('main')
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
     var ctx = canvas.getContext('2d');
-    var fork = {
-        img: forkImg,
-        x: 0,
-        y: 0,
-        draw: function(){
-            ctx.drawImage(this.img, this.x, this.y);
-        },
-        taken: false
-    };
+    var fork = new sprite (ctx, forkImg, 0, 0);
+    fork.taken = false;
     canvas.addEventListener('click', function(ev){
         if (!fork.taken && ev.clientX > fork.x && ev.clientX < fork.x + fork.img.naturalWidth
         && ev.clientY > fork.y && ev.clientY < fork.y + fork.img.naturalHeight)
@@ -29,12 +43,10 @@ function draw(){
     })
     canvas.addEventListener('mousemove', function(ev){
         if (fork.taken){
-            fork.x = ev.clientX;
-            fork.y = ev.clientY;
+            fork.move(ev.clientX, ev.clientY);
         }
     })
-    fork.x = 100;
-    fork.y = 100;
+    fork.move(100, 100);
     fork.draw();
     var anim = function(){
         if (fork.taken){
